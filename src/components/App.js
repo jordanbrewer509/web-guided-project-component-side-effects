c4ec011d198e6b30d397035e4dfa826c689b8365
Jordan~ <jordan.brewer509@gmail.com>
2022-12-29 13:48:13 -0800

    finished video

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // 👉 TASK 1 - import the axios lib from node_modules
-
+import axios from 'axios';
 // 👉 TASK 2 - import the contants from constants/index.js
 
+import { BASE_URL, API_KEY } from '../constants';
 import Details from './Details'
 import Friend from './Friend';
 
@@ -23,10 +24,20 @@ export default function App() {
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
 
+  useEffect(() => {
+    axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
+    .then(res => {
+      setFriends(res.data);
+    }).catch(err => console.log(err))
+  }, [])
+
   return (
     <div className='container'>
       <h1>Some of my friends:</h1>
       {/* start by mapping over the friends array...*/}
+      { friends.map(friend => {
+        return <Friend info={friend} key={friend.id} openDetails={openDetails}/>
+      })}
       {
         currentFriendId && <Details friendId={currentFriendId} close={closeDetails} />
       }
--- src/components/Details.js
+++ src/components/Details.js
@@ -8,16 +8,43 @@ export default function Details(props) {
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
 
+  useEffect(() => {
+    console.log('effect after the first dom surgery')
+  }, [])
+
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
 
+  useEffect(() => {
+    const sillyClickListener = () => {
+      console.log('heres a random num: ' + Math.floor(Math.random() * 10))
+    }
+    document.addEventListener('click', sillyClickListener);
+
+    return () => {
+      console.log('cleaning up the silly event listener');
+      document.removeEventListener('click', sillyClickListener)
+    }
+  }, [])
+
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+
+  useEffect(() => {
+    console.log('uh oh')
+  }, []);
 
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+    .then(res => {
+      setDetails(res.data);
+    }).catch(err => console.error(err))
+  }, [friendId])
 
   return (
     <div className='container'>
